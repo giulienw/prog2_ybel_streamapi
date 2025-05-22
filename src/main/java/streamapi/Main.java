1598a92e98a75d5c8f55b591f402d26c7ef71193
Giulien <giulienchow@gmail.com>
2025-05-22 17:10:38 +0200

    Changed Main#random to use stream api

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -58,21 +58,11 @@ public class Main {
      */
     public static List<Integer> random() {
         Random r = new Random();
-
-        // TODO
-        List<Integer> randomIntegers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            randomIntegers.add(r.nextInt(10));
-        }
-
-        List<Integer> returnList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            if (randomIntegers.get(i) % 2 == 0) {
-                returnList.add(randomIntegers.get(i) * randomIntegers.get(i));
-            }
-        }
-
-        return returnList;
+        return r.ints(10, 0, 10)
+            .filter(i -> i % 2 == 0)
+            .map(i -> i * i)
+            .boxed()
+            .toList();
     }
 
     /**
